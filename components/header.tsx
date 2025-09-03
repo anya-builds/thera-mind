a15c82533c45bdda4b1071e0233dab2eda8a3434
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-09-03 20:59:03 +0530

    theramind

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,11 @@
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div  className="w-full fixed top-0 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">Header
+        <div className="absolute inset-0 border-b border-primary/10" />
+  <header className="relative max-w-6xl mx-auto px-4">
+
+        </header>
     </div>
   )
 }
