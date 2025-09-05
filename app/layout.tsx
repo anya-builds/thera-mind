5ce4d226400c1e8724cc2cd1cf4fe670159c77ed
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-09-05 21:34:40 +0530

    theramind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+// import Header from "@/components/header";
 import { Providers } from "@/components/providers";
+import { Header } from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,7 @@ export default function RootLayout({
       >
         {/* Wrap everything inside Providers */}
         <Providers>
-          <Header />
+          <Header/>
           {children}
         </Providers>
       </body>
