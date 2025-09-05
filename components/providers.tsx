fcc63bdd06f4094ba0fa5b860bcc25fea6fd5905
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-09-05 21:08:10 +0530

    theramind

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-// import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
+import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-// import { SessionProvider as CustomSessionProvider } from "@/lib/contexts/session-context";
+import { SessionProvider as CustomSessionProvider } from "@/lib/contexts/session-context";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    
+    <NextAuthSessionProvider>
+      <CustomSessionProvider>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -15,6 +16,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         >
           {children}
         </ThemeProvider>
-    
+      </CustomSessionProvider>
+    </NextAuthSessionProvider>
   );
 }
